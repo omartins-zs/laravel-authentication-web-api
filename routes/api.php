eee20275344e40f2f0c643757d5db922cc74ef54
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-08 02:06:14 -0300

    Criando Rota User Profile Privada

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'profile']);
