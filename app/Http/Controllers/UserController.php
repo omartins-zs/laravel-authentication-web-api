982e889a87dac76e226b850d7a1806570b25f8e7
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-08 02:06:53 -0300

    Create metodo Profile com Dados do User Logado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    // Método para retornar os dados do usuário autenticado
+    public function profile(Request $request)
+    {
+        return response()->json(Auth::user());
+    }
 }
