0e5c342cbac09e9bf062c000b032db1051f09ad1
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-01 22:27:59 -0300

    Add a Rota dos Logs

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LogController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -9,3 +10,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'profile']);
+
+Route::get('/logs', [LogController::class, 'index']);
